41e1151d08eaa21e3d64f6673b29bd630dad4d78
alejandroestradam <alejoestradam@yahoo.com>
2020-10-19 17:37:16 -0500

    Se realizo el codigo de perimetro del hexagono

---
+++ app/src/main/java/com/edwinacubillos/practica420201/Fragment_Perimetro_Hexagono.kt
@@ -0,0 +1,38 @@
+package com.edwinacubillos.practica420201
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment__perimetro__hexagono.*
+
+
+class Fragment_Perimetro_Hexagono : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__perimetro__hexagono, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_calculo.setOnClickListener {
+            val lados= editTex_lados.text.toString().toInt()
+            val longitud=editTex_longitud.text.toString().toDouble()
+            var resultado:Double= lados*longitud
+            editTex_perimetro.setText(resultado.toString())
+
+        }
+
+    }
+
+    companion object {
+
+    }
+}
