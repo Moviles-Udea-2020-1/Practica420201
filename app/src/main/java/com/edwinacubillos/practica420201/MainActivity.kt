9e57aade36695643c068df87fd6b18d78e2fa537
Edwin A. Cubillos <edwinacubillos@gmail.com>
2020-10-19 16:29:04 -0500

    Creación de la rama ejemplo_branch

--- app/src/main/java/com/edwinacubillos/practica420201/MainActivity.kt
+++ app/src/main/java/com/edwinacubillos/practica420201/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //creando la rama develeop
+        //creando feature/ejemplo_branch
     }
 }
