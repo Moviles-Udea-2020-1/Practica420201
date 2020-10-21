535c241f08f62739f9370a0966d2164c8555dc6b
Dario Arevalo <daferd19@gmail.com>
2020-10-21 08:46:02 -0500

    crecacion de la rama paralelograma

--- app/src/main/java/com/edwinacubillos/practica420201/ui/main/MainActivity.kt
+++ app/src/main/java/com/edwinacubillos/practica420201/ui/main/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.edwinacubillos.practica420201
+package com.edwinacubillos.practica420201.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.edwinacubillos.practica420201.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //creando la rama develeop
-        //creado feature/paralelogramo.
+        //creado feature/area_paralelogramo
     }
 }
